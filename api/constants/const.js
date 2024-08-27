024ec49fe00d3f151092204c4c85696d05088046
dotuser <ahmedhassaan@tutanota.com>
2024-08-27 21:33:35 +0500

    Validate Phone Number

--- api/constants/const.js
+++ api/constants/const.js
@@ -1,1 +1,9 @@
 export const path = '/public/temp/';
+
+export const convertPhoneNumber = number => {
+    number = number.replace(/\D/g, '');
+    if (number.startsWith('03')) {
+        return '92' + number.slice(1);
+    }
+    return number;
+}
