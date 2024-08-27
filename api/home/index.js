cf9a251dc102d87bdbcde8a94bdde8525bd0efe7
dotuser <ahmedhassaan@tutanota.com>
2024-08-27 21:34:16 +0500

    Home/User Wifi Req Page

--- api/home/index.js
+++ api/home/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import path from 'path';
-// import { path } from '../constants/const.js';
+import { convertPhoneNumber } from '../constants/const.js';
+import sendWappMsg from '../webhooks/wapp/sendMessage.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../../public/temp') });
+  res.sendFile('index.html', 
+    { 
+      root: path.join(__dirname, '../../public/temp') 
+    }
+  );
+});
+
+router.post('/sendWifiPassword', (req, res) => {
+  const { name, email, wapp, dob } = req.body;
+  const contact = [];
+
+  console.log(req.body);
+  
+  if (wapp !== '') {
+    const formattedNumber = convertPhoneNumber(wapp);
+    contact.push(formattedNumber);
+    console.log(contact);
+
+    sendWappMsg(contact, 'Wifi Password');
+    res.status(200).send('Well Done')
+  }
 });
 
 export default router;
